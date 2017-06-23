6af2a12ff075097d760c55f9b9746091dbdd2f93
ss.pan <sorrowlikeblue@gmail.com>
2017-06-23 18:13:34 +0800

    fix some bugs

--- proj/app/src/main/java/com/armaccelerator/bleapcart/MainActivity.java
+++ proj/app/src/main/java/com/armaccelerator/bleapcart/MainActivity.java
@@ -1,85 +1,189 @@
 package com.armaccelerator.bleapcart;
 
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 import android.os.Handler;
 import android.os.Message;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity{
-    public String ble_mac = null;  //蓝牙BLE MAC
+
+    public String ble_mac = "00:0E:0B:13:C5:B4";  //蓝牙BLE MAC
     public String hub_mac = "CC:1B:E0:E0:25:90";  //HUB MAC
     public String hub_ip  = null;  //HUB IP
-    public boolean hub_FLAG = false;
 
-    private Button send_hub_mac,  send_ble_mac;
+    public static final int SHOW_RESPONSE = 0;
+
+    private Button send_hub_mac,  send_ble_mac, disconnect_ble;
     private ImageButton send_ahead, send_back, send_left, send_right, send_stop;
     private TextView tx_response;
+    private EditText editText;
 
-    private final int VIBRATE_TIME = 60;
+    //新建Handler,接收Message更新TextView控件的内容
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SHOW_RESPONSE:
+                    String response = (String) msg.obj;
+                    tx_response.setText(response);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    };
 
     private OnClickListener listener = new OnClickListener() {
         @Override
         public void onClick(View view) {
             try {
-                if (send_hub_mac == view) {
 
+                //HUB_MAC按键触发
+                int VIBRATE_TIME = 60;
+                if (send_hub_mac == view) {
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
 
-                    String result = null;
-                    String requestUrl = "http://api.cassianetworks.com/cassia/hubs/CC:1B:E0:E0:25:90";
-                    result = httpRequest.sendRequestWithHttpClient(requestUrl);
-                    Toast toast = Toast.makeText(MainActivity.this, String.format(""), Toast.LENGTH_SHORT);
-                    toast.show();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String response = cassiaHub.discoverHub(hub_mac);
+                            Message message = new Message();
+                            message.what = SHOW_RESPONSE;
+                            message.obj = response;
+                            handler.sendMessage(message);
+                            hub_ip = response;
+                        }
+                    }).start();
                 }
+
+                //按键触发
                 if (send_ble_mac == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
-
-                    Toast toast = Toast.makeText(MainActivity.this, String.format("连接失败，请检查HUB MAC：%s",hub_ip), Toast.LENGTH_SHORT);
-                    toast.show();
+                    ble_mac = editText.getText().toString();
+                    if(ble_mac.equals(""))
+                    {
+                        ble_mac = "00:0E:0B:13:C5:B4";
+                    }
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String response = cassiaHub.connectBle(ble_mac,hub_ip);
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("BLE连接状态：%s",response), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
                 }
-               if (send_ahead == view) {
+
+                //按键触发
+                if (disconnect_ble == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String result = cassiaHub.disconnectBle(ble_mac,hub_ip);
+                            if(result.equals("OK")) {
+                                Looper.prepare();
+                                Toast toast = Toast.makeText(MainActivity.this, "连接断开", Toast.LENGTH_SHORT);
+                                toast.show();
+                                Looper.loop();
+                            }
+                        }
+                    }).start();
+                }
+                if (send_ahead == view) {
+
+                    vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int index = cassiaHub.sendCommand(ble_mac,hub_ip,"ahead");
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("方向：%d",index), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
 
                 }
                 if (send_back == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int index = cassiaHub.sendCommand(ble_mac,hub_ip,"back");
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("方向：%d",index), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
 
                 }
                 if (send_left == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
-
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int index = cassiaHub.sendCommand(ble_mac,hub_ip,"left");
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("方向：%d",index), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
                 }
                 if (send_right == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int index = cassiaHub.sendCommand(ble_mac,hub_ip,"right");
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("方向：%d",index), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
 
                 }
                 if (send_stop == view) {
 
                     vibrateHelp.vSimple(view.getContext(), VIBRATE_TIME);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            int index = cassiaHub.sendCommand(ble_mac,hub_ip,"stop");
+                            Looper.prepare();
+                            Toast toast = Toast.makeText(MainActivity.this, String.format("方向：%d",index), Toast.LENGTH_SHORT);
+                            toast.show();
+                            Looper.loop();
+                        }
+                    }).start();
 
                 }
             }catch(Exception e){
 
             }
-
         }
     };
 
@@ -89,18 +193,24 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         send_hub_mac= (Button) this.findViewById(R.id.send_hub_mac_button);
         send_ble_mac = (Button) this.findViewById(R.id.send_ble_mac_button);
+        disconnect_ble = (Button) this.findViewById(R.id.discon_ble_mac_button);
         send_ahead = (ImageButton) this.findViewById(R.id.ahead);
         send_back = (ImageButton) this.findViewById(R.id.back);
         send_left = (ImageButton) this.findViewById(R.id.left);
         send_right = (ImageButton) this.findViewById(R.id.right);
         send_stop = (ImageButton) this.findViewById(R.id.stop);
+
         send_hub_mac.setOnClickListener(listener);
         send_ble_mac.setOnClickListener(listener);
+        disconnect_ble.setOnClickListener(listener);
         send_ahead.setOnClickListener(listener);
         send_back.setOnClickListener(listener);
         send_left.setOnClickListener(listener);
         send_right.setOnClickListener(listener);
         send_stop.setOnClickListener(listener);
+
         tx_response = (TextView)findViewById(R.id.tx_response_s);
+
+        editText = (EditText) findViewById(R.id.ble_mac);
     }
 }
--- proj/app/src/main/java/com/armaccelerator/bleapcart/cassiaHub.java
+++ proj/app/src/main/java/com/armaccelerator/bleapcart/cassiaHub.java
@@ -1,45 +1,206 @@
 package com.armaccelerator.bleapcart;
 
-import android.widget.Toast;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.HttpURLConnection;
-import java.net.InetAddress;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
-
-import org.json.JSONArray;
-import org.json.JSONException;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-
 
 public class cassiaHub {
 
     public  static String discoverHub(String hubMac){
+
         String hubIp = null;
-        String result = "00001";
+        String result = null;
+        String requestUrl = "http://api.cassianetworks.com/cassia/hubs/deviceId";
+        requestUrl = requestUrl.replace("deviceId",hubMac);
+
         try {
-            String requestUrl = "http://api.cassianetworks.com/cassia/hubs/CC:1B:E0:E0:25:90";
-            URL realUrl = new URL(requestUrl);
-            HttpURLConnection connection = (HttpURLConnection) realUrl.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setDoOutput(true);
-            if (connection.getResponseCode() == 200) {
-                result = "GET成功";
-            } else {
-                result = "GET失败";
-            }
-            return  result;
+            result = sendHttpGet(requestUrl);
+            JSONObject oJTObject = new JSONObject(result);
+            hubIp = (String) oJTObject.get("ip");
         }catch (Exception e)
         {
-            return  result;
         }
+        return hubIp;
+    }
+
+    public  static String connectBle(String bleMac,String hubIp){
+
+        String result = null;
+        String requestUrl = "http://deviceIp/gap/nodes/nodeId/connection?mac=&chip=";
+        requestUrl = requestUrl.replace("nodeId",bleMac);
+        requestUrl = requestUrl.replace("deviceIp",hubIp);
+        try {
+            result = sendHttpPost(requestUrl);
+        }catch (Exception e)
+        {
+        }
+        return result;
+    }
+
+    public  static String disconnectBle(String bleMac,String hubIp){
+
+        String result = null;
+        String requestUrl = "http://deviceIp/gap/nodes/nodeId/connection?mac=";
+        requestUrl = requestUrl.replace("nodeId",bleMac);
+        requestUrl = requestUrl.replace("deviceIp",hubIp);
+        try {
+            result = sendHttpDelete(requestUrl);
+        }catch (Exception e)
+        {
+        }
+        return result;
+    }
+
+    public  static int sendCommand(String bleMac,String hubIp, String comm){
+        String requestUrl = "http://deviceIp/gatt/nodes/nodeId/handle/16/value/comm_tx/?mac=";
+        requestUrl = requestUrl.replace("deviceIp",hubIp);
+        requestUrl = requestUrl.replace("nodeId",bleMac);
+        switch (comm) {
+            case "ahead":
+                String ahead_tx = "24312c302c302c302c302c302c302c302c302c302324302c302c302c302c302c302c302c302c302c3023";
+                try{
+                    String response = sendHttpGet(requestUrl.replace("comm_tx",ahead_tx));
+                    if (response != null)
+                        return 0;
+                    else
+                        return -1;
+                }catch (Exception e)
+                {
+
+                }
+                break;
+            case "back":
+                String back_tx = "24322c302c302c302c302c302c302c302c302c302324302c302c302c302c302c302c302c302c302c3023";
+                try{
+                    String response = sendHttpGet(requestUrl.replace("comm_tx",back_tx));
+                    if (response != null)
+                        return 1;
+                    else
+                        return -1;
+                }catch (Exception e)
+                {
+
+                }
+                break;
+            case "left":
+                String left_tx = "24332c302c302c302c302c302c302c302c302c302324302c302c302c302c302c302c302c302c302c3023";
+                try{
+                    String response = sendHttpGet(requestUrl.replace("comm_tx",left_tx));
+                    if (response != null)
+                        return 2;
+                    else
+                        return -1;
+                }catch (Exception e)
+                {
+
+                }
+                break;
+            case "right":
+                String right_tx = "24342c302c302c302c302c302c302c302c302c302324302c302c302c302c302c302c302c302c302c3023";
+                try{
+                    String response = sendHttpGet(requestUrl.replace("comm_tx",right_tx));
+                    if (response != null)
+                        return 3;
+                    else
+                        return -1;
+                }catch (Exception e)
+                {
+
+                }
+                break;
+            case "stop":
+                String stop_tx = "24302c302c302c302c302c302c302c302c302c3023";
+                try{
+                    String response = sendHttpGet(requestUrl.replace("comm_tx",stop_tx));
+                    if (response != null)
+                        return 4;
+                    else
+                        return -1;
+                }catch (Exception e)
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+        return -1;
+    }
+
+    //HTTP GET方法
+    public static  String sendHttpGet(String url) {
+        String result = null;
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            HttpResponse httpResponse = httpClient.execute(httpGet);
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity = httpResponse.getEntity();
+                String response = EntityUtils.toString(entity, "utf-8"); //将entity当中的数据转换为字符串
+                result = response.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    //HTTP POST方法
+    public static  String sendHttpPost(String url) {
+
+        String result = null;
+
+        try {
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("timeOut", "300");
+            jsonObj.put("type", "random");
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            httpPost.setHeader("Content-Type", "application/json");
+            HttpEntity requestEntity = new StringEntity(jsonObj.toString());
+            httpPost.setEntity(requestEntity);
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                if (response.getStatusLine().getStatusCode() == 200) {
+                    HttpEntity entity = response.getEntity();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(entity.getContent()));
+                    result = reader.readLine();
+                }
+            } catch (Exception e) {
+            }
+        } catch (Exception e) {
+        }
+        return result;
+    }
+
+    //HTTP DELETE方法
+    public static  String sendHttpDelete(String url) {
+        String result = null;
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpDelete httpDelete = new HttpDelete(url);
+        try {
+            HttpResponse response = httpClient.execute(httpDelete);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                HttpEntity entity = response.getEntity();
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(entity.getContent()));
+                result = reader.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
